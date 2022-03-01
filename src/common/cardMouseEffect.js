1b77ee8ae45c6470409aad728c6407e9925f1d59
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-03-01 21:06:19 +0530

    Update cardMouseEffect.js

--- src/common/cardMouseEffect.js
+++ src/common/cardMouseEffect.js
@@ -1,16 +1,17 @@
-import getSiblings from './getSiblings'
+// import getSiblings from './getSiblings'
 
-const cardMouseEffect = (featureEl) => {
-  var featuresitems = featureEl;
-  if (featuresitems) {
-    featuresitems.forEach((item) => {
-      item.onmouseover = function (event) {
-        item.classList.add("active");
-        let siblings = getSiblings(item),
-          siblingEl = siblings.map((e) => e.classList.remove("active"));
-      };
-    });
-  }
-};
+// const cardMouseEffect = (featureEl) => {
+//   var featuresitems = featureEl;
+//   if (featuresitems) {
+//     featuresitems.forEach((item) => {
+//       item.onmouseover = function (event) {
+//         item.classList.add("active");
+//         let siblings = getSiblings(item),
+//           siblingEl = siblings.map((e) => e.classList.remove("active"));
+//       };
+//     });
+//   }
+// };
 
-export default cardMouseEffect;
+// export default cardMouseEffect;
+
