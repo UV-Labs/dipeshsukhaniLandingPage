4e6aa69e0d1e5d4fe9d18d153e84f818c5eca493
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-03-01 22:20:52 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,16 +12,36 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          
           <meta
             name="keywords"
-            content="Dipesh Sukhani - Portfolio"
+            content="Dipesh Sukhani"
           />
           <meta
             name="description"
-            content=""
+            content="Dipesh Sukhani - Portfolio"
           />
-          <meta name="author" content="" />
+      {/* Facebook Meta Tags  */}
+        <meta property="og:url" content="https://dipeshsukhani.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Dipesh Sukhani" />
+        <meta property="og:description" content="Dipesh Sukhani - Portfolio" />
+        <meta
+          property="og:image"
+          content="https://www.dipeshsukhani.dev/img/hero.png"
+        />
+            
+            {/* Twitter Meta Tags  */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="dipeshsukhani.dev" />
+        <meta property="twitter:url" content="https://dipeshsukhani.dev/" />
+        <meta name="twitter:title" content="Dipesh Sukhani" />
+        <meta name="twitter:description" content="Dipesh Sukhani - Portfolio" />
+        <meta
+          name="twitter:image"
+          content="https://www.dipeshsukhani.dev/img/hero.png"
+        />
+          
           <link rel="shortcut icon" href="/img/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
